8ce386b5e3f9cbfdef7229d4f543ef73dbf6ca1c
javaknows <juanpserranor@gmail.com>
2021-11-06 17:32:52 -0400

    Update StorageSlotMock.sol

--- contracts/mocks/StorageSlotMock.sol
+++ contracts/mocks/StorageSlotMock.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../utils/StorageSlot.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/utils/StorageSlot.sol";
 
 contract StorageSlotMock {
     using StorageSlot for bytes32;
