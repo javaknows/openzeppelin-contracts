2298b523ecf54bea88af75572a973fb5e0a0194a
javaknows <juanpserranor@gmail.com>
2021-11-06 14:24:36 -0400

    Update ERC20Wrapper.sol

--- contracts/token/ERC20/extensions/ERC20Wrapper.sol
+++ contracts/token/ERC20/extensions/ERC20Wrapper.sol
@@ -3,8 +3,8 @@
 
 pragma solidity ^0.8.0;
 
-import "../ERC20.sol";
-import "../utils/SafeERC20.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol";
 
 /**
  * @dev Extension of the ERC20 token contract to support token wrapping.
