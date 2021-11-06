c1c806aee67e4dd821aa1943af9ddbf8319384f1
javaknows <juanpserranor@gmail.com>
2021-11-06 18:03:57 -0400

    Update ContextMock.sol

--- contracts/mocks/ContextMock.sol
+++ contracts/mocks/ContextMock.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../utils/Context.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/utils/Context.sol";
 
 contract ContextMock is Context {
     event Sender(address sender);
