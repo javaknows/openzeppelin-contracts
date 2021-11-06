c2a84b4fa64dbb486dd370fa9b25500b72b83ee3
javaknows <juanpserranor@gmail.com>
2021-11-06 18:12:56 -0400

    Update ERC165Mock.sol

--- contracts/mocks/ERC165Mock.sol
+++ contracts/mocks/ERC165Mock.sol
@@ -2,6 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-import "../utils/introspection/ERC165.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/utils/introspection/ERC165.sol";
 
 contract ERC165Mock is ERC165 {}
