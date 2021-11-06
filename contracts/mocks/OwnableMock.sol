64084c37ead44800972236f1756baf3842d7374d
javaknows <juanpserranor@gmail.com>
2021-11-06 17:40:29 -0400

    Update OwnableMock.sol

--- contracts/mocks/OwnableMock.sol
+++ contracts/mocks/OwnableMock.sol
@@ -2,6 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-import "../access/Ownable.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 contract OwnableMock is Ownable {}
