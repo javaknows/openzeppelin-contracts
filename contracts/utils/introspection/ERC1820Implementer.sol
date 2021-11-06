672cf4072899eeb367a7b6fdd861491b323bfb5a
javaknows <juanpserranor@gmail.com>
2021-11-06 10:31:39 -0400

    Update ERC1820Implementer.sol

--- contracts/utils/introspection/ERC1820Implementer.sol
+++ contracts/utils/introspection/ERC1820Implementer.sol
@@ -3,7 +3,7 @@
 
 pragma solidity ^0.8.0;
 
-import "./IERC1820Implementer.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/utils/introspection/IERC1820Implementer.sol";
 
 /**
  * @dev Implementation of the {IERC1820Implementer} interface.
