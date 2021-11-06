f41fccab2dca9c0b3e16c350d7429f34f7c8df9a
javaknows <juanpserranor@gmail.com>
2021-11-06 14:47:42 -0400

    Update GovernorVotes.sol

--- contracts/governance/extensions/GovernorVotes.sol
+++ contracts/governance/extensions/GovernorVotes.sol
@@ -3,9 +3,9 @@
 
 pragma solidity ^0.8.0;
 
-import "../Governor.sol";
-import "../../token/ERC20/extensions/ERC20Votes.sol";
-import "../../utils/math/Math.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/governance/Governor.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/utils/math/Math.sol";
 
 /**
  * @dev Extension of {Governor} for voting weight extraction from an {ERC20Votes} token.
