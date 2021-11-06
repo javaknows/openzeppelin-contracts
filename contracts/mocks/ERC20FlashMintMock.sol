0cc9ad62a54b6d98f2c80742321cdcc4ea5ee218
javaknows <juanpserranor@gmail.com>
2021-11-06 18:08:55 -0400

    Update ERC20FlashMintMock.sol

--- contracts/mocks/ERC20FlashMintMock.sol
+++ contracts/mocks/ERC20FlashMintMock.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../token/ERC20/extensions/ERC20FlashMint.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
 
 contract ERC20FlashMintMock is ERC20FlashMint {
     constructor(
