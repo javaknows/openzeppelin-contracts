0a1e3ce688d8b983c8375f9a9bcd84d2434ea03d
javaknows <juanpserranor@gmail.com>
2021-11-06 11:00:38 -0400

    Update EnumerableMap.sol

--- contracts/utils/structs/EnumerableMap.sol
+++ contracts/utils/structs/EnumerableMap.sol
@@ -3,7 +3,7 @@
 
 pragma solidity ^0.8.0;
 
-import "./EnumerableSet.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/utils/structs/EnumerableSet.sol";
 
 /**
  * @dev Library for managing an enumerable variant of Solidity's
