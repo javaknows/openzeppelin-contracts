25c18d152bb5bb22a00f8e15a246e9381d1962f3
javaknows <juanpserranor@gmail.com>
2021-11-06 17:41:54 -0400

    Update MulticallTokenMock.sol

--- contracts/mocks/MulticallTokenMock.sol
+++ contracts/mocks/MulticallTokenMock.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.0;
 
-import "../utils/Multicall.sol";
-import "./ERC20Mock.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/utils/Multicall.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/mocks/ERC20Mock.sol";
 
 contract MulticallTokenMock is ERC20Mock, Multicall {
     constructor(uint256 initialBalance) ERC20Mock("MulticallToken", "BCT", msg.sender, initialBalance) {}
