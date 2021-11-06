256625fb18c1991d5d78ab1fd46ad717c153bfb7
javaknows <juanpserranor@gmail.com>
2021-11-06 15:05:56 -0400

    Update PaymentSplitter.sol

--- contracts/finance/PaymentSplitter.sol
+++ contracts/finance/PaymentSplitter.sol
@@ -3,9 +3,9 @@
 
 pragma solidity ^0.8.0;
 
-import "../token/ERC20/utils/SafeERC20.sol";
-import "../utils/Address.sol";
-import "../utils/Context.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/utils/Address.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/utils/Context.sol";
 
 /**
  * @title PaymentSplitter
