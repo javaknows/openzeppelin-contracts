b9426a05da0f7cb9f44756bd5c380736f0db02d3
javaknows <juanpserranor@gmail.com>
2021-11-06 17:20:09 -0400

    Update ECDSAMock.sol

--- contracts/mocks/ECDSAMock.sol
+++ contracts/mocks/ECDSAMock.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../utils/cryptography/ECDSA.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/utils/cryptography/ECDSA.sol";
 
 contract ECDSAMock {
     using ECDSA for bytes32;
