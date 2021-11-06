dbfe5576cc539e8f4fe24aa16eac647fc7136086
javaknows <juanpserranor@gmail.com>
2021-11-06 18:02:06 -0400

    Update ERC721BurnableMock.sol

--- contracts/mocks/ERC721BurnableMock.sol
+++ contracts/mocks/ERC721BurnableMock.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../token/ERC721/extensions/ERC721Burnable.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract ERC721BurnableMock is ERC721Burnable {
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
