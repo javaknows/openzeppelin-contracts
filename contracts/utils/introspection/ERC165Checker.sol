e7208da85ba99280d3325cbdf73508de8e179a9b
javaknows <juanpserranor@gmail.com>
2021-11-06 10:30:52 -0400

    Update ERC165Checker.sol

--- contracts/utils/introspection/ERC165Checker.sol
+++ contracts/utils/introspection/ERC165Checker.sol
@@ -3,7 +3,7 @@
 
 pragma solidity ^0.8.0;
 
-import "./IERC165.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/token/ERC1155/IERC1155.sol";
 
 /**
  * @dev Library used to query support of an interface declared via {IERC165}.
