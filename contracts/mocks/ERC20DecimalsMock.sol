2961d26fefd3b1d68c4a6b31a42281858a68c020
javaknows <juanpserranor@gmail.com>
2021-11-06 18:09:18 -0400

    Update ERC20DecimalsMock.sol

--- contracts/mocks/ERC20DecimalsMock.sol
+++ contracts/mocks/ERC20DecimalsMock.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../token/ERC20/ERC20.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20DecimalsMock is ERC20 {
     uint8 private immutable _decimals;
