0820af560d4bda930100abba37e7df490f59bec9
javaknows <juanpserranor@gmail.com>
2021-11-06 10:29:57 -0400

    Update ERC165.sol

--- contracts/utils/introspection/ERC165.sol
+++ contracts/utils/introspection/ERC165.sol
@@ -3,7 +3,7 @@
 
 pragma solidity ^0.8.0;
 
-import "./IERC165.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/token/ERC1155/IERC1155.sol";
 
 /**
  * @dev Implementation of the {IERC165} interface.
