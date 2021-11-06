00714c4b4bc2c0ad4d30151c29b887d946b0caf7
javaknows <juanpserranor@gmail.com>
2021-11-06 17:32:28 -0400

    Update StringsMock.sol

--- contracts/mocks/StringsMock.sol
+++ contracts/mocks/StringsMock.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../utils/Strings.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/utils/Strings.sol";
 
 contract StringsMock {
     function fromUint256(uint256 value) public pure returns (string memory) {
