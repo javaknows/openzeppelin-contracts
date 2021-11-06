3fe590da0250d0624ad8a7c45942cfc35477b5f8
javaknows <juanpserranor@gmail.com>
2021-11-06 18:12:17 -0400

    Update ERC165StorageMock.sol

--- contracts/mocks/ERC165StorageMock.sol
+++ contracts/mocks/ERC165StorageMock.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../utils/introspection/ERC165Storage.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/utils/introspection/ERC165Storage.sol";
 
 contract ERC165StorageMock is ERC165Storage {
     function registerInterface(bytes4 interfaceId) public {
