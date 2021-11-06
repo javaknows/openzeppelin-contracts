990126efe182d02c1c74fd7b913429eb165ae5a4
javaknows <juanpserranor@gmail.com>
2021-11-06 18:00:30 -0400

    Update ERC721PausableMock.sol

--- contracts/mocks/ERC721PausableMock.sol
+++ contracts/mocks/ERC721PausableMock.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../token/ERC721/extensions/ERC721Pausable.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721Pausable.sol";
 
 /**
  * @title ERC721PausableMock
