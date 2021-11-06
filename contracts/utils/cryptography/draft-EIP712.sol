c79e8757af58ac58392015368e52affa9cd05716
javaknows <juanpserranor@gmail.com>
2021-11-06 12:11:33 -0400

    Update draft-EIP712.sol

--- contracts/utils/cryptography/draft-EIP712.sol
+++ contracts/utils/cryptography/draft-EIP712.sol
@@ -3,7 +3,7 @@
 
 pragma solidity ^0.8.0;
 
-import "./ECDSA.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/utils/cryptography/ECDSA.sol";
 
 /**
  * @dev https://eips.ethereum.org/EIPS/eip-712[EIP 712] is a standard for hashing and signing of typed structured data.
