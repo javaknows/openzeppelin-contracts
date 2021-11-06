61fa3c9dde8433d1c1ae979181e6ff9d201b76c7
javaknows <juanpserranor@gmail.com>
2021-11-06 17:43:20 -0400

    Update MerkleProofWrapper.sol

--- contracts/mocks/MerkleProofWrapper.sol
+++ contracts/mocks/MerkleProofWrapper.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../utils/cryptography/MerkleProof.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/utils/cryptography/MerkleProof.sol";
 
 contract MerkleProofWrapper {
     function verify(
