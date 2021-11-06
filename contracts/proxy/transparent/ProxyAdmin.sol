40e2cff5c5d800ad31d11354bfb12f009e49b2a1
javaknows <juanpserranor@gmail.com>
2021-11-06 14:32:48 -0400

    Update ProxyAdmin.sol

--- contracts/proxy/transparent/ProxyAdmin.sol
+++ contracts/proxy/transparent/ProxyAdmin.sol
@@ -3,8 +3,8 @@
 
 pragma solidity ^0.8.0;
 
-import "./TransparentUpgradeableProxy.sol";
-import "../../access/Ownable.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 /**
  * @dev This is an auxiliary contract meant to be assigned as the admin of a {TransparentUpgradeableProxy}. For an
