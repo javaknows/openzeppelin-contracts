1ced3a398c28e6847096d4340bb026b2c5e1fd5a
javaknows <juanpserranor@gmail.com>
2021-11-06 18:10:24 -0400

    Update ERC20BurnableMock.sol

--- contracts/mocks/ERC20BurnableMock.sol
+++ contracts/mocks/ERC20BurnableMock.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../token/ERC20/extensions/ERC20Burnable.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract ERC20BurnableMock is ERC20Burnable {
     constructor(
