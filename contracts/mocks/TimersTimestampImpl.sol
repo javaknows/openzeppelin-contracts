9921857b4bacf6b7cc966b24074fa9a988497d00
javaknows <juanpserranor@gmail.com>
2021-11-06 17:30:42 -0400

    Update TimersTimestampImpl.sol

--- contracts/mocks/TimersTimestampImpl.sol
+++ contracts/mocks/TimersTimestampImpl.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../utils/Timers.sol";
+import "https://github.com/javaknows/openzeppelin-contracts/blob/master/contracts/utils/Timers.sol";
 
 contract TimersTimestampImpl {
     using Timers for Timers.Timestamp;
